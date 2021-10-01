56ec8979d33b281274dbfa1043160330f90494ce
basalcode <basalcode@gmail.com>
2021-10-01 13:03:58 +0900

    Add interface on typescript

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,22 @@ sayHi(name, age, gender);
 sayHi(name, age); // typescript에서는 애러가 발생한다.
 */
 
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, gender{gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Alex",
+  age: 20,
+  gender: "male"
+}
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`;
 };
 
-console.log(sayHi("Alex", 20, "male"));
+console.log(sayHi(person));
 
 export {};
